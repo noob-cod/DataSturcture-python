d45b41876e8036d32a9e8ce03c49d50064561c7c
noob-cod <zc532693536@163.com>
2021-04-12 16:02:34 +0800

    红黑树（删除节点部分未完成）、堆（未完成）

---
+++ Tree/Heap.py
@@ -0,0 +1,54 @@
+"""
+@Date: 2021/4/9 下午1:37
+@Author: Chen Zhang
+@Brief: 堆 的实现
+
+堆的定义：
+堆的特性：
+"""
+
+
+class Heap:
+    def __init__(self):
+        """Initiator, create a new heap."""
+        pass
+
+    def is_Empty(self):
+        """Return whether self is empty"""
+        return 0
+
+    def __len__(self):
+        """Return the amount of nodes of self."""
+        return 0
+
+    def __str__(self):
+        """Return the string of self."""
+        return ''
+
+    def __iter__(self):
+        """Yield items in self with specific order"""
+        pass
+
+    def __add__(self, other):
+        """Compose two Heap object"""
+        pass
+
+    def __eq__(self, other):
+        """Return whether self is equal to other Heap obj."""
+        return 0
+
+    def add(self, newNode):
+        """Add new node to self."""
+        pass
+
+    def delete(self, target):
+        """Delete specific node from self"""
+        pass
+
+    def clear(self):
+        """Reset self"""
+        pass
+
+
+if __name__ == '__main__':
+    pass
