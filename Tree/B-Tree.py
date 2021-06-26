00228c27a019ed857ea0c0b5a796748d57f09a15
noob-cod <zc532693536@163.com>
2021-06-26 12:12:01 +0800

    实现了具备简单插入和删除功能的B树。删除方法仍然有优化的空间

--- Tree/B-Tree.py
+++ Tree/B-Tree.py
@@ -24,7 +24,7 @@
             1）提取节点中第(m//2)个关键字，向上插入到父节点中（递归判断是否需要继续触发节点拆分）；
             2）将被提取关键字左右的关键字分别存放到两个节点中；
             3）将新关键字插入对应节点。
-    3.2 借节点
+    3.2 借关键字
         3.2.1 场景：在B树的删除操作中，当待删除关键字在叶子节点中，且当前该叶子节点的关键字数量已经达到下限时触发借节点
         3.2.2 步骤：
             1）从父节点中转移一个合适的关键字到当前节点中；
@@ -83,6 +83,27 @@ class BNode:
         self._keys_nums += 1
         return index
 
+    def delete_key(self, target):
+        """删除关键字"""
+        self.__delete_key(target)
+
+    def __delete_key(self, target):
+        index = self.search(target)
+        if index == -1:
+            raise ValueNotFound(target)
+
+        if index == self._keys_nums - 1:
+            self._keys.pop()
+        else:
+            self._keys[index:] = self._keys[index+1:]
+        self._keys_nums -= 1
+
+        # 合并子节点
+        if self._sons_nums:
+            new_node = BNode(sourceCollection=(self.sons[index].keys + self.sons[index+1].keys))
+            self.sons[index] = new_node
+            self.__delete_node(index + 1)
+
     def add_node(self, newNode, index=None):
         """
         添加新的子节点，并返回该子节点的索引
@@ -98,13 +119,18 @@ class BNode:
     def __add_node(self, newNode, index=None):
         if not index or (index == self.sons_nums):
             self._sons.append(newNode)
-            self._sons_nums += 1
         # 添加位置在非末尾的位置时
         else:
             self._sons[index+1:] = self._sons[index:]  # 后移
             self._sons[index] = newNode
-            self._sons_nums += 1
+        self._sons_nums += 1
         newNode.father = self
+
+    def __delete_node(self, index):
+        """删除指定位置的子节点，慎用，不会触发B树的修复机制"""
+        assert index < self._sons_nums
+        self._sons[index:] = self._sons[index+1:]
+        self._sons_nums -= 1
 
     def printKeys(self):
         """打印节点关键字"""
@@ -115,10 +141,13 @@ class BNode:
 
     def search(self, target):
         """搜索目标关键字，返回索引"""
+        return self.__search(target)
+
+    def __search(self, target):
         if self.keys_nums == 0:
             return -1
         # 当关键字数量不大于4的情况下使用顺序查找
-        elif self.keys_nums <= 2:
+        if self.keys_nums <= 4:
             for index in range(self.keys_nums):
                 if self._keys[index] == target:
                     return index
@@ -135,6 +164,20 @@ class BNode:
                 else:
                     right = mid - 1
             return -1
+
+    def search_node(self, target):
+        """搜索子节点，返回索引"""
+        return self.__search_node(target)
+
+    def __search_node(self, target):
+        if self._sons_nums == 0:
+            return -1
+        index = 0
+        while index < self._sons_nums:
+            if self._sons[index] == target:
+                return index
+            index += 1
+        return -1
 
     @property
     def father(self):
@@ -181,6 +224,9 @@ class BTree:
         self._root = None  # 根节点
         self._node_number = 0  # B树中的节点树
 
+        self._least_key_number_inner = (ceil(self._order / 2) - 1)
+        self._least_son_number_inner = ceil(self._order / 2)
+
         if sourceCollection:
             for item in sourceCollection:
                 self.add(item)
@@ -211,7 +257,7 @@ class BTree:
         index = None
         if root.sons_nums:
             index = root.sons_nums - 1
-            while index >= (root.sons_nums // 2):
+            while (root.sons_nums // 2) <= index:
                 self.__printInorder(root.sons[index], height + 1, length)
                 index -= 1
         string = 'keys: ' + str(root.keys)
@@ -244,20 +290,108 @@ class BTree:
 
             # 判断当前节点关键字数量是否超出阶数限制。若超出，则触发节点拆分进行修复，否则不进行任何操作。
             while cursor.keys_nums > self.order - 1:
-                self.__separate_node(cursor)
+                self.__split_node(cursor)
                 cursor = cursor.father
                 if cursor is None:
                     break
 
     def delete(self, target):
         """删除关键字"""
-        # if cursor == self._root:
-        #     least_keys = 1
-        #     least_sons = 2
-        # else:
-        #     least_keys = int(ceil(self.order / 2.0) - 1)
-        #     least_sons = int(ceil(self.order / 2.0))
-        pass
+        self.__delete(target)
+
+    def __delete(self, target):
+        # 定位target
+        node = self.__find(target, location=True)
+        index = node.search(target)
+        if index == -1:
+            raise ValueNotFound(target)
+
+        # 判断待删值情况
+
+        # 待删关键字不是叶子节点，则与其后继关键字值交换位置后，删除后继关键字
+        if node.sons_nums != 0:
+            cursor = node.sons[index + 1]
+            while cursor.sons_nums:
+                cursor = cursor.sons[0]
+            node.keys[index], cursor.keys[0] = cursor.keys[0], node.keys[index]  # 交换关键字位置
+            node, index = cursor, 0
+
+        # 删除这个关键字
+        node.delete_key(node.keys[index])
+
+        # 若待删节点为根节点且关键字为空，则重置B树
+        if self.root == node and node.keys_nums == 0:
+            self.root = None
+            self._node_number = 0
+            return
+
+        # 删除后B树的修复
+        cursor = node
+        while True:
+            if self.root == cursor:
+                break
+            # 若当前节点关键字数量低于下限，则需要修复B树
+            if cursor.keys_nums < self._least_key_number_inner:
+                me_index = cursor.father.search_node(cursor)  # 当前节点在父节点的子节点中的索引
+                # 若存在关键字数量大于下限的兄弟节点，则触发借关键字操作
+                if self.__rich_brother(cursor) != -1:
+                    brother, brother_index = self.__rich_brother(cursor)  # 可借的兄弟节点及其索引
+                    if me_index < brother_index:
+                        cursor.add_key(cursor.father.keys[me_index])
+                        cursor.father.keys[me_index] = brother.keys[0]
+                        brother.delete_key(brother.keys[0])
+                    else:
+                        cursor.add_key(cursor.father.keys[me_index-1])
+                        cursor.father.keys[me_index-1] = brother.keys[-1]
+                        brother.delete_key(brother.keys[-1])
+
+                # 否则触发节点合并操作
+                else:
+                    # 若当前节点索引为0，则与其右侧节点合并
+                    if me_index == 0:
+                        brother_index = 1
+                        brother = cursor.father.sons[brother_index]
+                        new_collections = cursor.keys + [cursor.father.keys[0]] + brother.keys
+                        new_node = BNode(sourceCollection=new_collections)
+                        if cursor.sons_nums or brother.sons_nums:  # 继承子节点
+                            for item in cursor.sons[:]:
+                                new_node.add_node(item)
+                            for item in brother.sons[:]:
+                                new_node.add_node(item)
+                        cursor.father.delete_key(cursor.father.keys[0])
+
+                        # 这里可能出现BNode.delete_key()方法已经完成了根节点与子节点的合并，造成cursor.father.sons_nums为1的情况
+                        if cursor.father.sons_nums == 1:
+                            cursor.father.sons[0] = new_node
+                        else:
+                            cursor.father.sons[1] = new_node
+                            cursor.father.sons[:] = cursor.father.sons[1:]
+
+                    # 若当前节点索引不为0，则与其左侧节点合并
+                    else:
+                        brother_index = me_index - 1
+                        brother = cursor.father.sons[brother_index]
+                        # 当阶数较低时，这里可能出现cursor.keys_nums为0的情况
+                        if cursor.keys_nums:
+                            new_collections = brother.keys + [cursor.father.keys[brother_index]] + cursor.keys
+                        else:
+                            new_collections = brother.keys + [cursor.father.keys[brother_index]]
+                        new_node = BNode(sourceCollection=new_collections)
+                        if cursor.sons_nums or brother.sons_nums:  # 继承子节点
+                            for item in brother.sons[:]:
+                                new_node.add_node(item)
+                            for item in cursor.sons[:]:
+                                new_node.add_node(item)
+                        cursor.father.delete_key(cursor.father.keys[brother_index])  # 内部合并子节点
+                        cursor.father.sons[brother_index] = new_node
+                    if cursor.father.keys_nums == 0:
+                        self.root = new_node
+                        break
+                    else:
+                        new_node.father = cursor.father
+                cursor = cursor.father
+            else:
+                break
 
     def find(self, target):
         """查找关键字，并返回节点和索引"""
@@ -276,23 +410,49 @@ class BTree:
                       加入这个参数是为了在self.find()中复用self.__find()的代码。
         """
         cursor = self._root
-        while cursor.sons_nums:
-            if newItem < cursor.keys[0]:
-                cursor = cursor.sons[0]
-            elif location and newItem > cursor.keys[-1]:  # 短路计算
-                cursor = cursor.sons[cursor.sons_nums - 1]
-            elif newItem >= cursor.keys[-1]:
-                cursor = cursor.sons[cursor.sons_nums - 1]
-            else:
-                if location:
-                    return cursor
+        if not location:
+            while cursor.sons_nums:
+                if newItem < cursor.keys[0]:
+                    cursor = cursor.sons[0]
+                elif newItem >= cursor.keys[-1]:
+                    cursor = cursor.sons[cursor.sons_nums - 1]
+                else:
+                    index = 0
+                    while cursor.keys[index] <= newItem:
+                        index += 1
+                    cursor = cursor.sons[index]
+        else:
+            while cursor.sons_nums:
                 index = 0
-                while cursor.keys[index] <= newItem:
-                    index += 1
+                while index <= (cursor.keys_nums-1):
+                    if cursor.keys[index] == newItem:
+                        return cursor
+                    if cursor.keys[index] < newItem:
+                        index += 1
+                    else:
+                        break
                 cursor = cursor.sons[index]
         return cursor
 
-    def __separate_node(self, node):
+    def __rich_brother(self, node):
+        """返回指定节点的关键字数量大于下限的兄弟节点和其索引，若不存在则返回-1"""
+        if self.root == node:
+            return -1
+
+        # 定位node在父节点子节点中的索引index
+        father = node.father
+        index = father.search_node(node)
+
+        # index取到非0值时
+        if index != 0 and father.sons[index-1].keys_nums > self._least_key_number_inner:
+            return father.sons[index-1], index-1
+        # index取到非子节点数量-1时
+        elif index != (father.sons_nums - 1) and father.sons[index+1].keys_nums > self._least_key_number_inner:
+            return father.sons[index + 1], index + 1
+        else:
+            return -1
+
+    def __split_node(self, node):
         """节点拆分"""
         father = node.father
         # 提取的关键字的左侧关键字组成新子节点left，右侧关键字组成新子节点right
@@ -318,6 +478,9 @@ class BTree:
             insert_pos = father.add_key(node.keys[ind])  # 提取关键字，并入父节点，得到插入位置的索引
             father.add_node(left, insert_pos)
             father.sons[insert_pos+1], right.father = right, father  # 完成子节点的变化
+
+    def __combine_node(self, node):
+        pass
 
     @property
     def order(self):
@@ -355,7 +518,17 @@ class SonNumberOverflowError(BTreeError):
         print("当前节点子节点数量超出节点阶数")
 
 
+class ValueNotFound(BTreeError):
+    """目标值未找到异常"""
+    def __init__(self, item):
+        self.item = item
+
+    def __str__(self):
+        print('目标值{}不存在！'.format(self.item))
+
+
 if __name__ == '__main__':
+    # BNode测试
     # newNode1 = BNode()
     # newNode1.add_key(3)
     # newNode1.printKeys()
@@ -365,6 +538,15 @@ if __name__ == '__main__':
     # newNode1.printKeys()
     # print(newNode1.sons_nums)
 
-    btree = BTree(5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26])
+    # B树测试
+    order = 5
+    source_collections = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+    btree = BTree(order, source_collections)
     btree.printBTree()
-    pass
+    btree.delete(20)
+    btree.printBTree()
+    btree.delete(19)
+    btree.printBTree()
+    btree.delete(18)
+    btree.printBTree()
